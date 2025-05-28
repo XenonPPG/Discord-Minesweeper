d5fe0d2d2e5c22b5a6360cbb7cd85a22715e3650
Art <popugajarcik@gmail.com>
2025-05-28 16:03:50 +0300

    i18n fix

--- src/i18n.js
+++ src/i18n.js
@@ -1,9 +1,13 @@
 import { createI18n } from 'vue-i18n'
-import messages from '@intlify/unplugin-vue-i18n/messages'
+import en from '@/locales/en.json';
+import ru from '@/locales/ru.json';
 
 export const i18n = createI18n({
   legacy: false,
+  globalInjection: true,
   locale: 'en',
   fallbackLocale: 'en',
-  messages
+  messages: {
+    en, ru
+  }
 })
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueI18n from '@intlify/unplugin-vue-i18n/vite'
 import path from 'path'
 
 export default defineConfig({
   base: '/Discord-Minesweeper/',
   plugins: [
     vue(),
-    vueI18n({
-      include: path.resolve(__dirname, './src/locales/**')
-    })
   ],
   resolve: {
     alias: {
